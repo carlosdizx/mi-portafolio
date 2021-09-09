cfe2abeb6fded5eb716518514d3630ae66f2d767
carlosdizx <carlodiaz@umariana.edu.co>
2021-09-09 09:56:13 -0500

    Instalado y configurado firebase

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,24 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+import firebase from "firebase/app";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJjIoMaidY3P0baYUwha89UFnsJ9RWelc",
+  authDomain: "portafolio-carlos.firebaseapp.com",
+  projectId: "portafolio-carlos",
+  storageBucket: "portafolio-carlos.appspot.com",
+  messagingSenderId: "1078052358069",
+  appId: "1:1078052358069:web:15bf90b374b0426fc3bbfc",
+  measurementId: "G-43VHNY882Y"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+Vue.config.productionTip = false;
+
+export const STORAGE = firebase.storage();
 
 new Vue({
   router,
